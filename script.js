5f425efef6a55c2fcbe65cbd0bb8e41139565c6d
Sheikh-fatima02 <154690172+Sheikh-fatima02@users.noreply.github.com>
2026-03-06 23:12:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function showMessage(){
+alert("Your message has been sent successfully!");
+}
+window.addEventListener("scroll", function(){
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(function(card){
+
+let position = card.getBoundingClientRect().top;
+
+let screen = window.innerHeight;
+
+if(position < screen){
+card.style.opacity = "1";
+card.style.transform = "translateY(0)";
+}
+
+});
+
+});
